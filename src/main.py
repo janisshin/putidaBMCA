751b4ed6c1a6b8d4f411d41ab7d04cfd192f33a5
janisshin <janiseshin@gmail.com>
2024-05-01 14:48:02 -0700

    changed default setting for --iter

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--runName", metavar="FILENAME", type=str,
                         help="output as a pickle file")
-    parser.add_argument("--iter", metavar="N", type=int, default=8,
+    parser.add_argument("--iter", metavar="N", type=int, default=50000,
                         help="how many iterations to run")
     args = parser.parse_args()
     
